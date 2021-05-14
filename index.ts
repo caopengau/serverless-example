97dd128cf259d62d2b709c09ea114e95054412b6
Cao Peng <caopengau@gmail.com>
2021-05-15 01:10:55 +1000

    promise serverless-http

--- index.ts
+++ index.ts
@@ -1,4 +1,18 @@
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyEventV2,
+  Context
+} from "aws-lambda";
 import serverless from "serverless-http";
 import app from "./src/app";
 
-module.exports.handler = serverless(app);
+const handler = serverless(app);
+module.exports.handler = async (
+  event: APIGatewayProxyEvent | APIGatewayProxyEventV2,
+  context: Context
+) => {
+  // you can do other things here
+  const result = await handler(event, context);
+  // and here
+  return result;
+};
